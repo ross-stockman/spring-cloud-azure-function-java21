c5c288c7d4b0e1238d9e3028f9cab598d46678d6
Ross Stockman <ross@rstockman.org>
2022-12-27 19:00:12 -0600

    Initial commit

---
+++ src/main/java/dev/stockman/functions/ServerlessFunctions.java
@@ -0,0 +1,39 @@
+package dev.stockman.functions;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+@Configuration
+public class ServerlessFunctions {
+
+    private static final Logger LOG = LoggerFactory.getLogger(ServerlessFunctions.class);
+
+    @Bean
+    public Function<Mono<String>, Mono<String>> hello() {
+        return (name) -> name
+                .doOnSuccess(m -> LOG.info("Received event >> " + m))
+                .map(n -> String.format("%s was here!", n));
+    }
+
+    @Bean
+    public Supplier<Mono<String>> time() {
+        return () -> Mono.just("very late")
+                .doOnSuccess(m -> LOG.info("Received event >> " + m))
+                .map(m -> String.format("The time is %s!", m));
+    }
+
+    @Bean
+    public Consumer<Mono<String>> publish() {
+        return (message) -> message
+                .doOnSuccess(m -> LOG.info("Received event >> " + m))
+                .subscribe();
+    }
+
+}
